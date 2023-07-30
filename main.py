12428f748574ca6b826bd063c0d2420fcc42b862
Saeed Physics <sdmi13790@gmail.com>
2023-07-30 14:56:07 +0330

    add simple plot function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import telebot
+
 from environs import Env
+import matplotlib.pyplot as plt
+import numpy as np
+import numexpr
 
 env = Env()
 env.read_env()
@@ -13,13 +17,26 @@ def send_start(message):
     bot.send_message(message.chat.id, 'wellcome to my first bot 😊')
 
 
-key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-key_markup.add('one', 'two', 'three')
+key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+key_markup.add('x**2', 'two', 'three')
 
 
 @bot.message_handler(commands=['help'])
 def send_start(message):
-    bot.reply_to(message, 'I do physics calculation', reply_markup=key_markup)
+    bot.reply_to(message, 'I do computational physics calculation', reply_markup=key_markup)
+
+
+@bot.message_handler()
+def key_bord_message(message):
+    if message.text == 'x**2':
+        # bot.send_message(message.chat.id, "Please post a simple function to draw the graph")
+        x = np.linspace(-5, 5, 100)
+        y = numexpr.evaluate(message.text)
+        plt.plot(x, y, 'r')
+        plt.savefig('plot_name.png', dpi=300)
+        bot.send_photo(message.chat.id, photo=open('plot_name.png', 'rb'))
+    else:
+        bot.send_message(message.chat.id, "It will be added in the future")
 
 
 bot.infinity_polling()
