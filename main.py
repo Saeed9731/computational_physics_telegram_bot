1b2789da5deda5c2255cf0fdbb73802d51f9bc4f
Saeed Physics <sdmi13790@gmail.com>
2023-08-01 21:20:19 +0330

    add new ReplyKeyboardMarkup

--- main.py
+++ main.py
@@ -14,16 +14,32 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_start(message):
-    bot.send_message(message.chat.id, 'wellcome to my first bot 😊')
+    bot.send_message(message.chat.id, 'wellcome to my first bot')
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, r'I do computational physics calculation: , please use this comment: '
+                          '  /example'
+                          '/electromagnetic')
 
 
 key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
 key_markup.add('x**2', 'two', 'three')
 
 
-@bot.message_handler(commands=['help'])
-def send_start(message):
-    bot.reply_to(message, 'I do computational physics calculation', reply_markup=key_markup)
+@bot.message_handler(commands=['example'])
+def send_example(message):
+    bot.reply_to(message, 'this is our example', reply_markup=key_markup)
+
+
+key_markup2 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+key_markup2.add('one', 'two', 'three', 'four', 'five')
+
+
+@bot.message_handler(commands=['electromagnetic'])
+def send_electromagnetic(message):
+    bot.reply_to(message, 'this is our example', reply_markup=key_markup2)
 
 
 @bot.message_handler()
@@ -33,10 +49,19 @@ def key_bord_message(message):
         x = np.linspace(-5, 5, 100)
         y = numexpr.evaluate(message.text)
         plt.plot(x, y, 'r')
+        plt.grid()
+        plt.xlabel('x')
+        plt.ylabel(message.text)
         plt.savefig('plot_name.png', dpi=300)
         bot.send_photo(message.chat.id, photo=open('plot_name.png', 'rb'))
-    else:
-        bot.send_message(message.chat.id, "It will be added in the future")
+    elif message.text == 'two':
+        bot.send_message(message.chat.id, 'nice1-three')
+    elif message.text == 'three':
+        bot.send_message(message.chat.id, 'nice2-three')
+    elif message.text == 'four':
+        bot.send_message(message.chat.id, 'nice3-four')
+    elif message.text == 'five':
+        bot.send_message(message.chat.id, 'nice4-five')
 
 
 bot.infinity_polling()
